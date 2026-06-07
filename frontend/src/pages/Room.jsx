7023dba2233b87d03a18e5d752c720904d1ce3b4
Ainz-alt <viveksinghyadav626@gmail.com>
2026-06-07 15:05:40 +0530

    Comment

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -36,6 +36,7 @@ function Room() {
       navigate("/login");
       return;
     }
+    //commmit
     const payload = JSON.parse(atob(token.split(".")[1]));
     const uid = parseInt(payload.sub);
     userIdRef.current = uid;
